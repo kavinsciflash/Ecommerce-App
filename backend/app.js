fdb304aac6294efee67117f7038a4e6a86c4bdda
kavinprasanth <73733462+kavinsciflash@users.noreply.github.com>
2023-10-08 17:22:47 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use("/api/v1/product", productRoute);
 
 //rest api
 app.use('*', function (req, res) {
-    const index = path.join(__dirname, 'build', '../frontend/build/index.html');
+    const index = path.dirname('../frontend/build/index.html');
     res.sendFile(index);
   });
 
