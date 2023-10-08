8af0e28dd7d23128fcbabfb86895cc7369f723c7
kavinprasanth <73733462+kavinsciflash@users.noreply.github.com>
2023-10-08 14:58:47 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use("/api/v1/product", productRoute);
 
 //rest api
 app.use("*",function(req,res){
-    res.sendFile(path.join(__dirname, '..frontend/build/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
 const PORT=process.env.PORT || 8080;
