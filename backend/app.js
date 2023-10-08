ee5bb824e72cb9ec4f351846e5e9463f6396c5fd
kavinprasanth <kavinprasanth7274@gmail.com>
2023-10-08 17:18:48 +0530

    third commit

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ import categoryRoute from "./routes/categoryRoute.js";
 import productRoute from "./routes/productRoute.js"
 import cors from 'cors'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
 
 //configure env
 dotenv.config();
@@ -15,6 +17,8 @@ dotenv.config();
 connectDB();
 
 const app= express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //middlewares
 app.use(express.json())
