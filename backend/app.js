a9652b2ae41e3442cf4c24db5f8f34005da47fc6
kavinprasanth <kavinprasanth7274@gmail.com>
2023-10-08 14:51:46 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors"
+import morgan from 'morgan';
+import connectDB from "./config/db.js";
+import authRoute from "./routes/authRoute.js";
+import categoryRoute from "./routes/categoryRoute.js";
+import productRoute from "./routes/productRoute.js"
+import cors from 'cors';
+import path from 'path';
+
+//configure env
+dotenv.config();
+
+connectDB();
+
+const app= express();
+const __dirname = path.resolve();
+
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(express(path.join(__dirname, '../frontend/build')))
+//routes
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/category", categoryRoute);
+app.use("/api/v1/product", productRoute);
+
+//rest api
+app.use("*",function(req,res){
+    res.sendFile(path.join(__dirname, '..frontend/build/index.html'));
+});
+
+const PORT=process.env.PORT || 8080;
+
+app.listen(PORT,()=>{
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`);
+});
