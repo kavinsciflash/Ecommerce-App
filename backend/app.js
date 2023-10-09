5462c72be9120743d332c1a2ce1161a2690ab017
kavinprasanth <73733462+kavinsciflash@users.noreply.github.com>
2023-10-09 12:44:18 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,13 +17,13 @@ dotenv.config();
 connectDB();
 
 const app= express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 //middlewares
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express(path.join(__dirname, './frontend/build')))
+// app.use(express(path.join(__dirname, './frontend/build')))
 
 //routes
 app.use("/api/v1/auth", authRoute);
@@ -37,8 +37,12 @@ app.use("/api/v1/product", productRoute);
     // res.sendFile('../frontend/build/index.html', { root: __dirname });
   // });
 
-route.get("/", function(request, response) {
-  response.sendFile(__dirname + "./frontend/build/index.html");
+// route.get("/", function(request, response) {
+//   response.sendFile(__dirname + "./frontend/build/index.html");
+// });
+
+app.get("/",(req,res)=>{
+    res.send("<h1>welcome to ecommerce app</h1>");
 });
 
 
